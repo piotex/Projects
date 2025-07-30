5742efd24b889514d6751c31e479bdbdcb64d489
piotex <pkubon2@gmail.com>
2025-07-30 02:42:53 +0200

    cors fix

--- Python_React_CICD/backend/app.py
+++ Python_React_CICD/backend/app.py
@@ -5,7 +5,7 @@ import datetime
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "http://192.168.56.110"}})
+CORS(app)
 app.config.from_object(Config) 
 
 @app.route('/', methods=['GET'])
