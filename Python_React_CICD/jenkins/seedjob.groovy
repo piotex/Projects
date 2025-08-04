74d913060c69eba88be407529e487c1374e0c918
piotex <pkubon2@gmail.com>
2025-08-05 00:38:44 +0200

    fin

--- Python_React_CICD/jenkins/seedjob.groovy
+++ Python_React_CICD/jenkins/seedjob.groovy
@@ -1,13 +1,42 @@
-pipeline('my-github-pipeline') {
-    definition {
-        scm {
-            git {
-                remote {
-                    url('https://github.com/piotex/Projects.git')
-                }
-                branch('main')
-            }
+multibranchPipelineJob('multibranch-pipeline') {
+    displayName('multibranch-pipeline')
+    triggers {
+        periodicFolderTrigger {
+            interval('10m')
         }
-        scriptPath('Python_React_CICD/jenkins/example.Jenkinsfile')
+    }
+    branchSources {
+        git {
+            id('multibranch-pipeline')
+            remote('https://github.com/piotex/Projects.git')
+            includes('main')
+        }
+    }
+    factory {
+        workflowBranchProjectFactory {
+            scriptPath('Python_React_CICD/jenkins/example.Jenkinsfile')
+        }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(7)
+        }
+    }
+}
+pipelineJob("deploy") {
+    displayName('deploy')
+    description("Deploys.")
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/piotex/Projects.git')
+                    }
+                    branch('*/main')
+                }
+            }
+            scriptPath('Python_React_CICD/jenkins/example.Jenkinsfile')
+        }
     }
 }
